9d071722469cd027bfbbbccc20017af199376a44
Fernando Bevilacqua <dovyski@gmail.com>
2020-02-06 16:12:53 -0300

    Add initial laravel structure

--- public/index.php
+++ public/index.php
@@ -1,27 +1,60 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+define('LARAVEL_START', microtime(true));
 
-$reader->setReadDataOnly(TRUE);
-$spreadsheet = $reader->load("test.xlsx");
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
 
-$worksheet = $spreadsheet->getActiveSheet();
+require __DIR__.'/../vendor/autoload.php';
 
-echo '<table>' . PHP_EOL;
-foreach ($worksheet->getRowIterator() as $row) {
-    echo '<tr>' . PHP_EOL;
-    $cellIterator = $row->getCellIterator();
-    $cellIterator->setIterateOnlyExistingCells(FALSE); // This loops through all cells,
-                                                       //    even if a cell value is not set.
-                                                       // By default, only cells that have a value
-                                                       //    set will be iterated.
-    foreach ($cellIterator as $cell) {
-        echo '<td>' .
-             $cell->getValue() .
-             '</td>' . PHP_EOL;
-    }
-    echo '</tr>' . PHP_EOL;
-}
-echo '</table>' . PHP_EOL;
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
